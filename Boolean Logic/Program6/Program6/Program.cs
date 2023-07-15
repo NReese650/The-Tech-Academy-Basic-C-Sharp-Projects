4b8d67169b1ef92a276fcf5785beaedbaf609bc5
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-14 17:22:14 -0700

    coursework
    completing coursework

---
+++ Boolean Logic/Program6/Program6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Nick";
+            //string quote = "The man said \"Hello\", Nick.";
+            //string fileName = "C:\Users\Nick";
+
+            //bool trueOrFalse = name.Caontains("i");
+            //trueOrFalse = name.EndsWith("k");
+
+            //name = name.ToUpper();
+
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Nick");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ String/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+namespace StringManipulationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Concatenating three strings
+            string string1 = "Hello";
+            string string2 = "world";
+            string string3 = "!";
+
+            string concatenatedString = string1 + " " + string2 + string3;
+
+            Console.WriteLine("Concatenated String: " + concatenatedString);
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            // Converting a string to uppercase
+            string lowercaseString = "hello world";
+            string uppercaseString = lowercaseString.ToUpper();
+
+            Console.WriteLine("Uppercase String: " + uppercaseString);
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            // Building a paragraph using StringBuilder
+            StringBuilder paragraphBuilder = new StringBuilder();
+
+            // Append the first sentence
+            paragraphBuilder.Append("This is the first sentence. ");
+            Console.WriteLine(paragraphBuilder.ToString());
+
+            // Wait for the user to press a key
+            Console.ReadLine();
+
+            // Append the second sentence
+            paragraphBuilder.Append("This is the second sentence. ");
+            Console.WriteLine(paragraphBuilder.ToString());
+
+            // Wait for the user to press a key
+            Console.ReadLine();
+
+            // Append the third sentence
+            paragraphBuilder.Append("This is the third sentence.");
+            Console.WriteLine(paragraphBuilder.ToString());
+
+            // Wait for the user to press a key
+            Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+    
+}
