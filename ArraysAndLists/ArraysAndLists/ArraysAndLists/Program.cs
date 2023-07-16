6f8c44f9c9a7f8fea357ae11d61c43fb0b6e2830
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-16 10:20:57 -0700

    Coursework

---
+++ ArraysAndLists/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("Nick");
+        intList.Remove("Nick");
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+        byte[] byteArray = 
+    }
+}
---
+++ ArraysAndLists/ConsoleAppArray/ConsoleAppArray/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a one-dimensional array of strings
+        string[] stringArray = { "Apple", "Banana", "Orange", "Grapes" };
+
+        // Ask the user to select an index of the array
+        Console.WriteLine("Select an index of the string array (0-3): ");
+        int stringIndex = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the selected index is valid
+        if (stringIndex >= 0 && stringIndex < stringArray.Length)
+        {
+            // Display the string at the selected index
+            Console.WriteLine("Selected string: " + stringArray[stringIndex]);
+        }
+        else
+        {
+            // Display an error message for invalid index
+            Console.WriteLine("Invalid index selected.");
+        }
+
+        // Create a one-dimensional array of integers
+        int[] intArray = { 10, 20, 30, 40 };
+
+        // Ask the user to select an index of the array
+        Console.WriteLine("Select an index of the integer array (0-3): ");
+        int intIndex = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the selected index is valid
+        if (intIndex >= 0 && intIndex < intArray.Length)
+        {
+            // Display the integer at the selected index
+            Console.WriteLine("Selected integer: " + intArray[intIndex]);
+        }
+        else
+        {
+            // Display an error message for invalid index
+            Console.WriteLine("Invalid index selected.");
+        }
+
+        // Create a list of strings
+        List<string> stringList = new List<string> { "Cat", "Dog", "Elephant", "Lion" };
+
+        // Ask the user to select an index of the list
+        Console.WriteLine("Select an index of the string list (0-3): ");
+        int listIndex = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the selected index is valid
+        if (listIndex >= 0 && listIndex < stringList.Count)
+        {
+            // Display the content at the selected index
+            Console.WriteLine("Selected string from list: " + stringList[listIndex]);
+        }
+        else
+        {
+            // Display an error message for invalid index
+            Console.WriteLine("Invalid index selected.");
+        }
+    }
+}
