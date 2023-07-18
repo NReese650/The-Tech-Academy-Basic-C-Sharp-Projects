85f1b7df96954727beb0da2983c7f5a86ed17858
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-18 14:47:20 -0700

    Coursework

---
+++ Methods/MainMethod/MainMethod/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class BasicOperations
+    {
+        // Method to perform math operations on an integer
+        public int Operation(int number)
+        {
+            // Perform some math operation on the integer
+            int result = number + 10;
+            return result;
+        }
+
+        // Method to perform math operations on a decimal
+        public int Operation(decimal number)
+        {
+            // Perform a different math operation on the decimal
+            int result = (int)(number * 2);
+            return result;
+        }
+
+        // Method to perform math operations on a string (if it represents an integer)
+        public int Operation(string numberString)
+        {
+            int number;
+            if (int.TryParse(numberString, out number))
+            {
+                // Perform a different math operation on the parsed integer
+                int result = number - 5;
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. The provided string is not a valid integer.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Methods/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicOperations math = new BasicOperations();
+
+            // Call the first method, passing in an integer
+            int intResult = math.Operation(10);
+            Console.WriteLine("Integer Result: " + intResult);
+
+            // Call the second method, passing in a decimal
+            int decimalResult = math.Operation(3.14m);
+            Console.WriteLine("Decimal Result: " + decimalResult);
+
+            // Call the third method, passing in a string
+            int stringResult = math.Operation("20");
+            Console.WriteLine("String Result: " + stringResult);
+
+            Console.ReadLine();
+        }
+    }
+}
