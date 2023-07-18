1bc66191ff83f2d4294c3763ef252089a780cfc8
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-18 14:54:18 -0700

    coursework

---
+++ Methods/MethodClass/MethodClass/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Void
+{
+    public class MathOperations
+    {
+        // Void method to perform math operation on the first integer and display the second integer
+        public void Operation(int number1, int number2)
+        {
+            // Perform some math operation on the first integer (e.g., multiplication)
+            int result = number1 * 2;
+
+            // Display the second integer
+            Console.WriteLine("Second Integer: " + number2);
+        }
+    }
+}
---
+++ Methods/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Void
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            // Call the method in the class, passing in two numbers
+            math.Operation(5, 10);
+
+            // Call the method in the class, specifying the parameters by name
+            math.Operation(number1: 7, number2: 20);
+
+            Console.ReadLine();
+        }
+    }
+}
