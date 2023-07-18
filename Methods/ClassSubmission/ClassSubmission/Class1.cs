db32724c1dcd9a801a86f2ec1fc767ba6141e83e
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-18 16:14:40 -0700

    Coursework

---
+++ Methods/ClassSubmission/ClassSubmission/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Divide
+{
+    public void DivideByTwo(int data, out int result)
+    {
+        result = data / 2;
+        Console.WriteLine(result);
+    }
+    public static void DivideByTwo(double data, out double result)
+    {
+        result = data / 2.0;
+    }
+}
---
+++ Methods/ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class Submission Assignment");
+
+            //Istantiate the divide class
+            Divide output = new Divide();
+
+            // Prompt the user for input
+            Console.Write("Enter a number: ");
+
+            // Read the input from the user
+            string input = Console.ReadLine();
+
+            // Parse the input string to an integer
+            if (int.TryParse(input, out int number))
+            {
+                // Call the DivideByTwo method on the entered number
+                output.DivideByTwo(number, out int result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            // Wait for user input before exiting the program
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
