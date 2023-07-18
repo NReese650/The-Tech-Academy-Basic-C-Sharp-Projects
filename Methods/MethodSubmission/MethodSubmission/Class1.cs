2da2a028ad59802b0f3f6a647eea4741fd314d0b
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-18 14:51:24 -0700

    Coursework

---
+++ Methods/MethodSubmission/MethodSubmission/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    public class MathOperations
+    {
+        // Method to perform math operations on two integers
+        public int Operation(int number1, int number2 = 0)
+        {
+            // Perform some math operation on the numbers
+            int result = number1 + number2;
+            return result;
+        }
+    }
+}
---
+++ Methods/MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            // Ask the user for the first number
+            Console.Write("Enter the first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user for the second number
+            Console.Write("Enter the second number (optional): ");
+            string number2Input = Console.ReadLine();
+            int number2 = 0;
+
+            // Parse the second number if provided
+            if (!string.IsNullOrWhiteSpace(number2Input))
+                number2 = Convert.ToInt32(number2Input);
+
+            // Call the method in the class, passing in the numbers
+            int result = math.Operation(number1, number2);
+
+            // Display the result
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
