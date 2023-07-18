331a85113ad8ed8f7e096c663d7d9efd08fe3979
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-17 19:24:08 -0700

    Coursework

---
+++ Methods/CallingMethods/CallingMethods/MathMethods.cs
@@ -0,0 +1,26 @@
+﻿namespace MathApp
+{
+    public class MathMethods
+    {
+        // Method to perform addition operation on the received parameter
+        public int Add(int number)
+        {
+            int result = number + 10;
+            return result;
+        }
+
+        // Method to perform subtraction operation on the received parameter
+        public int Subtract(int number)
+        {
+            int result = number - 5;
+            return result;
+        }
+
+        // Method to perform multiplication operation on the received parameter
+        public int Multiply(int number)
+        {
+            int result = number * 2;
+            return result;
+        }
+    }
+}
---
+++ Methods/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number to perform math operations on
+            Console.Write("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Create an instance of the MathMethods class
+            MathMethods math = new MathMethods();
+
+            // Call each method, passing the user input, and display the returned result
+            int result1 = math.Add(userInput);
+            Console.WriteLine("Addition result: " + result1);
+
+            int result2 = math.Subtract(userInput);
+            Console.WriteLine("Subtraction result: " + result2);
+
+            int result3 = math.Multiply(userInput);
+            Console.WriteLine("Multiplication result: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
