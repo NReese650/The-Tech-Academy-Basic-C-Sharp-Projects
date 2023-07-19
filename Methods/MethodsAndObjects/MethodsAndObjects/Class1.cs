b302f48deef0ca231a2db01f707e4266e130a882
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-19 12:03:27 -0700

    coursework
    completing coursework

---
+++ Methods/MethodsAndObjects/MethodsAndObjects/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace MethodsAndObjects
+{
+    // Create a class called Person
+    class Person
+    {
+        // Define properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Define a method that writes the person's full name to the console
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    // Create another class called Employee and inherit from the Person class
+    class Employee : Person
+    {
+        // Add a property specific to the Employee class
+        public int Id { get; set; }
+    }
+}
---
+++ Methods/MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize an Employee object
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 123
+            };
+
+            // Call the superclass method SayName() on the Employee object
+            employee.SayName();
+
+            Console.ReadKey();
+        }
+    }
+}
