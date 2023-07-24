dc780fda9c24d0055d7d7a112043a8c292c58391
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-24 14:13:41 -0700

    Coursework
    Completing coursework

---
+++ Methods/Struct/Struct/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    // Create a struct called Number with the property "Amount" of data type decimal
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Methods/Struct/Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of data type Number
+            Number myNumber = new Number();
+
+            // Assign an amount to the Amount property of the Number object
+            myNumber.Amount = 123.45M;
+
+            // Print the amount to the console
+            Console.WriteLine("Amount: " + myNumber.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
