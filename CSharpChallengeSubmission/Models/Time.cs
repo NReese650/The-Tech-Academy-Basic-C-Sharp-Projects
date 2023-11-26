35ffb70bbaabb10704c7bd9711c9d1522d5a152a
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-11-25 20:15:48 -0800

    Challenge

---
+++ CSharpChallengeSubmission/Models/Time.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpChallengeSubmission.Models
+{
+    public class Time
+    {
+        public string CurrentTime { get; set; }
+
+        public Time()
+        {
+            CurrentTime = DateTime.Now.ToString("F"); 
+        }
+    }
+}
